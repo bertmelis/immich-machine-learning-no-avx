65386d568c0b46d8d05b4b727e6a103203ecb4f7
Bert Melis <bertmelis@users.noreply.github.com>
2022-11-23 18:21:02 +0100

    build for generic arch + simplify
    Former-commit-id: 185474f1919560f2fde515e298a7333d3d53a32e

--- build-tfjs/entrypoint.sh
+++ build-tfjs/entrypoint.sh
@@ -2,13 +2,9 @@
 
 cd output
 git clone https://github.com/tensorflow/tensorflow.git
-if cd tensorflow
-  then git pull
-else
-  git clone https://github.com/tensorflow/tensorflow.git
-  cd tensorflow
-fi
+cd tensorflow
 git checkout v3.19.0
 ./configure
-bazel build --config=mkl -c opt --copt=-march=native --config=monolithic //tensorflow/tools/lib_package:libtensorflow
+#bazel build --config=mkl -c opt --copt=-march=native --config=monolithic //tensorflow/tools/lib_package:libtensorflow
+bazel build --config=mkl -c opt --copt=-march=generic --config=monolithic //tensorflow/tools/lib_package:libtensorflow
 cp bazel-bin/tensorflow/tools/lib_package/libtensorflow.tar.gz /tensorflow/output/libtensorflow.tar.gz
