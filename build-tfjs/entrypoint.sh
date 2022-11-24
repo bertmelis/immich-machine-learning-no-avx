51fb989b4efdcd07c86d6a839c41edf957432abb
Bert Melis <bertmelis@users.noreply.github.com>
2022-11-24 10:02:37 +0100

    properly set build options to generic
    Former-commit-id: 04a9b4a3dfaa0aa33116fb4ad07d684c88992b79
    Former-commit-id: 2563e742ddc972d4a9960e14db08b203f20240ca

--- build-tfjs/entrypoint.sh
+++ build-tfjs/entrypoint.sh
@@ -6,5 +6,5 @@ cd tensorflow
 git checkout v2.11.0
 ./configure
 #bazel build --config=mkl -c opt --copt=-march=native --config=monolithic //tensorflow/tools/lib_package:libtensorflow
-bazel build --config=mkl -c opt --copt=-march=generic --config=monolithic //tensorflow/tools/lib_package:libtensorflow
+bazel build --config=mkl -c opt --copt=-march=x86-64 --copt-mtune=generic --config=monolithic //tensorflow/tools/lib_package:libtensorflow
 cp bazel-bin/tensorflow/tools/lib_package/libtensorflow.tar.gz /tensorflow/output/libtensorflow.tar.gz
